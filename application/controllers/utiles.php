6c457208fa1eba4dfbca49bb10146ee70cf503d3
Arteaga2k <cav1662@hotmail.com>
2015-03-03 11:35:08 +0100

    cambios importacion de ficheros

--- application/controllers/utiles.php
+++ application/controllers/utiles.php
@@ -9,6 +9,8 @@ require_once APPPATH . 'third_party/phpexcel/Classes/PHPExcel/IOFactory.php';
 class Utiles extends CI_Controller
 {
 
+    private $form = array();
+
     /**
      * Constructor clase Carro
      */
@@ -20,33 +22,29 @@ class Utiles extends CI_Controller
         $this->load->model('producto_model');
     }
 
-    public function importaDatosExcel()
-    {}
-
-    public function importaDatosFromXml()
-    {
-        $this->importaCategoriasXml();
-        $this->importaProductosXml();
-    }
-
     /**
-     * Llamada a funciones exportadatos
-     * categorias y productos
+     * Muestra formulario para subir un archivo
      */
-    public function exportaDatosXml()
+    public function importa_datos()
     {
-        $this->exportaCategoriasXml();
-        $this->exportaProductosXml();
+        $this->form["form_upload"] = form_open_multipart('utiles/do_upload', array(
+            "class" => "form-horizontal",
+            "name" => "procesaFormUsuario"
+        ));
+        
+        echo $this->twig->render('ficheros/upload_formulario.twig', array(
+            'form' => $this->form
+        ));
     }
 
     /**
      * Importación listado categorías desde un fichero excel
      * a la bbdd
      */
-    public function importaCategoriasExcel()
+    public function importa_categorias_excel($nombre)
     {
         $categorias = array();
-        $inputFileName = dirname(__DIR__) . '/upload' . '/' . 'categoria.xlsx';
+        $inputFileName = 'uploads/' . $nombre;
         
         try {
             $objPHPExcel = PHPExcel_IOFactory::load($inputFileName);
@@ -70,14 +68,57 @@ class Utiles extends CI_Controller
     }
 
     /**
+     * Importación listado categorías desde un fichero excel
+     * a la bbdd
+     */
+    public function importa_productos_excel($nombre)
+    {
+        $categorias = array();
+        $inputFileName = 'uploads/' . $nombre;
+        
+        try {
+            $objPHPExcel = PHPExcel_IOFactory::load($inputFileName);
+        } catch (PHPExcel_Reader_Exception $e) {
+            die('Error loading file "' . pathinfo($inputFileName, PATHINFO_BASENAME) . '": ' . $e->getMessage());
+        }
+        
+        $sheetData = $objPHPExcel->getActiveSheet()->toArray(null, true, true, true);
+        
+        // recorremos por fila excel
+        foreach ($sheetData as $data) {
+            $id = $this->producto_model->addProducto(array(
+                'idProducto' => $data['A'],
+                'nombre' => $data['B'],
+                'codigo' => $data['C'],
+                'precio' => $data['D'],
+                'imagen' => $data['E'],
+                'revisado' => $data['F'],
+                'descuento' => $data['G'],
+                'anuncio' => $data['H'],
+                'descripcion' => $data['I'],
+                'iva' => $data['J'],
+                'stock' => $data['K'],
+                'idCategoria' => $data['L']
+            ));
+        }
+    }
+
+    /**
      * Importa listado categorias contenido en categorias.xml
      * a la bbdd
      */
-    public function importaCategoriasXml()
+    public function importa_categorias_xml($nombre)
     {
-        $categorias = simplexml_load_file(dirname(__DIR__) . '/upload' . '/' . 'categorias.xml');
+        $categorias = simplexml_load_file('uploads/' . $nombre);
         foreach ($categorias as $categoria) {
-            echo $categoria->nombre;
+            $id = $this->categoria_model->addCategoria(array(
+                'idCategoria' => $categoria->idCategoria,
+                'nombre' => $categoria->nombre,
+                'codigo' => $categoria->codigo,
+                'descripcion' => $categoria->descripcion,
+                'anuncio' => $categoria->anuncio,
+                'revisado' => $categoria->revisado
+            ));
         }
     }
 
@@ -85,10 +126,9 @@ class Utiles extends CI_Controller
      * Importa lista productos desde un fichero xml
      * a la bbdd
      */
-    public function importaProductosXml()
+    public function importa_productos_xml($nombre)
     {
-        $productos = simplexml_load_file(dirname(__DIR__) . '/upload' . '/' . 'categorias.xml');
-        var_dump($productos);
+        $productos = simplexml_load_file('uploads/' . $nombre);
         
         foreach ($productos as $producto) {
             $this->producto_model->addProducto(array(
@@ -104,16 +144,25 @@ class Utiles extends CI_Controller
                 'iva' => $producto->iva,
                 'stock' => $producto->stock,
                 'idCategoria' => $producto->idCategoria
-            )
-            );
+            ));
         }
+    }
+
+    /**
+     * Llamada a funciones exportadatos
+     * categorias y productos
+     */
+    public function exporta_xml()
+    {
+        $this->exporta_categorias_xml();
+        $this->exporta_productos_xml();
     }
 
     /**
      * Exporta la tabla Categorias de la bbdd
      * a un fichero xml, categorias.xml
      */
-    public function exportaCategoriasXml()
+    public function exporta_categorias_xml()
     {
         // obtenemos lista categorías
         $categorias = $this->home_model->getCategorias();
@@ -133,14 +182,14 @@ class Utiles extends CI_Controller
         $xml .= "</categorias>";
         $sxe = new SimpleXMLElement($xml);
         // exportamos lista categorías a fichero xml
-        $sxe->asXML(dirname(__DIR__) . '/upload' . '/' . 'categorias.xml');
+        $sxe->asXML('uploads/categorias.xml');
     }
 
     /**
-     * Exporta la tabla productos de la bbdd 
+     * Exporta la tabla productos de la bbdd
      * en un fichero xml, productos.xml
      */
-    public function exportaProductosXml()
+    public function exporta_productos_xml()
     {
         // obtenemos lista categorias
         $productos = $this->producto_model->getProductos();
@@ -165,6 +214,91 @@ class Utiles extends CI_Controller
         $xml .= "</productos>";
         $sxe = new SimpleXMLElement($xml);
         // exportamos categorias a fichero formato xml
-        $sxe->asXML(dirname(__DIR__) . '/upload' . '/' . 'productos.xml');
+        $sxe->asXML('uploads/productos.xml');
+    }
+
+    /**
+     * Procesa y valida la subida de un fichero xml o excel
+     * Si todo es correcto, realiza la importanción del mismo
+     */
+    function do_upload()
+    {
+        // configuramos parámetros libreria upload
+        $config['upload_path'] = 'uploads/';
+        $config['allowed_types'] = 'xml|xlsx';
+        $config['max_size'] = '2000';
+        $config['max_width'] = '2024';
+        $config['max_height'] = '2008';
+        
+        $this->load->library('upload', $config);
+        
+        if (! $this->upload->do_upload('filename')) {
+            $this->form['error'] = $this->upload->display_errors();
+            $this->importa_xml();
+        } else {
+            
+            // obtenemos info del archivo subido
+            $file_info = $this->upload->data();
+            $nombre = $file_info['file_name'];
+            $tipo = $file_info['file_type'];
+            
+            $pos = strpos($tipo, 'sheet');
+            
+            if ($pos === false) {
+                // inicia la importación del fichero subido según contenido
+                $this->identifica_contenido_xml($nombre);
+                $icono = 'xml.png';
+            } else {
+                // inicia la importación del fichero subido según contenido
+                $this->identifica_contenido_excel($nombre);
+                $icono = 'xlsx.png';
+            }
+            
+            // mostramos mensaje subida correcta
+            echo $this->twig->render('ficheros/uploads_vista.twig', array(
+                'tipo' => $icono,
+                'nombre' => $nombre
+            ));
+        }
+    }
+
+    /**
+     * Identifica tipo (productos, categorias...) del fichero xml
+     * y lee contenido correspondiente
+     *
+     * @param unknown $nombre            
+     */
+    function identifica_contenido_xml($nombre)
+    {
+        $productos = simplexml_load_file('uploads/' . $nombre);
+        
+        switch ($productos->getName()) {
+            case 'productos':
+                $this->importa_productos_xml($nombre);
+                break;
+            case 'categorias':
+                $this->importa_categorias_xml($nombre);
+                break;
+            default: // TODO
+        }
+    }
+
+    /**
+     * Identifica tipo (productos, categorias...) del fichero excel
+     * y lee contenido correspondiente
+     *
+     * @param unknown $nombre            
+     */
+    function identifica_contenido_excel($nombre)
+    {
+        switch ($nombre) {
+            case 'productos':
+                $this->importa_productos_excel($nombre);
+                break;
+            case 'categorias':
+                $this->importa_categorias_excel($nombre);
+                break;
+            default: // TODO
+        }
     }
 }
